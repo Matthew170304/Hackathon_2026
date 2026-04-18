804ee0a828c717de3e5fcd7ee45c4aa358ccdd5f
Matthew170304 <milav24@student.sdu.dk>
2026-04-18 13:41:16 +0200

    fix

--- app/services/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.services.language_service import LanguageService
-from app.services.translation_service import TranslationService
-
-__all__ = [
-    "LanguageService",
-    "TranslationService",
-]
